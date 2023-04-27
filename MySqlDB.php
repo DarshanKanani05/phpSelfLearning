200c70ad5551130312f472c688d73dad614f8b57
Darshan Kanani <darshan.kanani.05@gmail.com>
2023-04-27 19:41:36 +0530

    MySqlDB

---
+++ MySqlDB.php
@@ -0,0 +1,25 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password);
+
+if (!$conn) {
+    die("Sorry we failed to connect : " . mysqli_connect_error());
+} else {
+    echo "Connection Was Done!!!<br>";
+}
+
+$sql = "CREATE DATABASE MySqlDB2";
+$result = mysqli_query($conn, $sql);
+
+if($result){
+    echo "The db was created successfully!!!";
+}
+else{
+    echo "The db was not created successfully!!! The Error is ".mysqli_error($conn);
+}
+
+?>
