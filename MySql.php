f532056464b0dcdf99e145335f79d556451f12dd
Darshan Kanani <darshan.kanani.05@gmail.com>
2023-04-27 19:20:07 +0530

    mysql

---
+++ MySql.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password);
+
+if (!$conn) {
+    die("Sorry we failed to connect : " . mysqli_connect_error());
+} else {
+    echo "Connection Was Done!!!";
+}
+
+?>
